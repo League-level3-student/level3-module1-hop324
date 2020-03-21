251f6cfec3477c57aae16300b93c7c41e9962403
hop324 <39838785+hop324@users.noreply.github.com>
2020-03-20 17:04:03 -0700

    IPod Shuffle
    I'm so bored. This quarantine is killing me.

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,12 +1,18 @@
 package _01_IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 //Copyright The League of Amazing Programmers, 2015
 
-public class _06_IPodShuffle{
+public class _06_IPodShuffle implements ActionListener{
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
 				
@@ -20,8 +26,36 @@ public class _06_IPodShuffle{
 		 */
 		
 	}
-	
+	public JFrame frame = new JFrame();
+	public JPanel panel = new JPanel();
+	public JButton button = new JButton();
+	public static ArrayList<Song> songs = new ArrayList<Song>();
+	public int spiciest;
 	public static void main(String[] args) {
-		new _06_IPodShuffle();
+		_06_IPodShuffle shuffle = new _06_IPodShuffle();
+		Song demo = new Song("demo.mp3");
+		Song thingy = new Song("/Users/thoma/Downloads/Wolves.mp3");
+		songs.add(demo);
+		songs.add(thingy);
+		new _06_IPodShuffle().createUI();
+	}
+	
+	public void createUI() {
+		frame.add(panel);
+		panel.add(button);
+		button.setText("Surprise me");
+		button.addActionListener(this);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		songs.get(spiciest).stop();
+		Random randy = new Random();
+		int spicy = randy.nextInt(2);
+		spiciest = spicy;
+		System.out.println(spicy);
+		songs.get(spicy).play();
 	}
 }

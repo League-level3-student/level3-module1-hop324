db31083d2cd6522c5eaf8761469b4061d71556b5
hop324 <39838785+hop324@users.noreply.github.com>
2020-04-07 22:22:47 -0700

    Finished Mod 1
    :+1: :sleepy:

--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,9 +1,24 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class _02_LogSearch {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_LogSearch implements ActionListener{
+	public JFrame frame = new JFrame();
+	public JPanel panel = new JPanel();
+	public JButton button1 = new JButton();
+	public JButton button2 = new JButton();
+	public JButton button3 = new JButton();
+	public JButton button4 = new JButton();
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
+	 
 	 * Create a GUI with three buttons. 
 	 * Button 1: Add Entry
 	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
@@ -29,5 +44,59 @@ public class _02_LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
+	HashMap<Integer, String> boi = new HashMap<Integer, String>();
+	public int highestID = 0;
 	
+	public static void main(String [] args) {
+		new _02_LogSearch().createUI();
+	}
+	
+	public void createUI() {
+		panel.add(button1); panel.add(button2); panel.add(button3); panel.add(button4);
+		frame.add(panel);
+		button1.addActionListener(this);
+		button1.setText("Add Entry");
+		button2.addActionListener(this);
+		button2.setText("Search by ID");
+		button3.addActionListener(this);
+		button3.setText("View List");
+		button4.addActionListener(this);
+		button4.setText("Remove Entry:");
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == button1) {
+			String id = JOptionPane.showInputDialog("Please register an ID number");
+			String name = JOptionPane.showInputDialog("Please register a name to accompany the given ID");
+			if(highestID < Integer.parseInt(id)) {
+				highestID = Integer.parseInt(id);
+			}
+			boi.put(Integer.parseInt(id), name);
+		}
+		if(e.getSource() == button2) {
+			String idsearch = JOptionPane.showInputDialog("Please enter an ID to search for");
+			System.out.println(boi.get(Integer.parseInt(idsearch)));
+		}
+		if(e.getSource() == button3) {
+			for(int i = 0; i < highestID+1; i++) {
+				if(boi.containsKey(i)) {
+					System.out.println("ID: " + i + " Name: " + boi.get(i));
+				}
+			}
+		}
+		if(e.getSource() == button4) {
+			String idsearch = JOptionPane.showInputDialog("Please enter an ID to remove");
+			if(boi.containsKey(Integer.parseInt(idsearch))) {
+				boi.remove(Integer.parseInt(idsearch));
+				System.out.println("Successfully removed the given ID");
+			}
+			else {
+				System.out.println("Sorry, this is not a valid ID");
+			}
+		}
+		
+	}
 }
